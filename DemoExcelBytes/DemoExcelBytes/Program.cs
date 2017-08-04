1d104c4284d45ecb4221dfc1bf0e8de0d41cbb58
cvalderrama <cvalderrama@riafinancial.com>
2017-08-04 10:33:10 -0700

    Pruebas de Excel con bytes

---
+++ DemoExcelBytes/DemoExcelBytes/Program.cs
@@ -0,0 +1,40 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExcelBytes
+{
+    class Program
+    {
+        const string fileName = "prueba2.xlsx";
+        static void Main(string[] args)
+        {
+            //Cargar datos (arreglo de bytes)
+            var data = GetData();
+
+            //Guardar en archivo Excel (./bin/debug/)
+            File.WriteAllBytes(fileName, data);
+
+            //Cargar Objeto Archivo 
+            var xlFileInfo = new FileInfo(fileName);
+
+            //Leer Celdas de Excel en memoria
+            using (ExcelPackage xlPackage = new ExcelPackage(xlFileInfo))
+            {
+                //Leer Hoja 1
+                ExcelWorksheet worksheet = xlPackage.Workbook.Worksheets[1];
+                //Leer los 2 valores de Excel
+                Console.WriteLine($"Valor A1: { worksheet.Cells["A1"].Value}");
+                Console.WriteLine($"Valor A4: { worksheet.Cells["A4"].Value}");
+            }
+        }
+        public static byte[] GetData()
+        {
+            return DatosPrueba.Data;
+        }
+    }
+}
